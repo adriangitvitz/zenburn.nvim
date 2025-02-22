37c29a31e26dcd6dc2375f1ce81b49cbebc0fe10
Adrian Najera <39295224+adriangitvitz@users.noreply.github.com>
2025-02-22 00:02:20 -0600

    mactch section c

--- lua/lualine/themes/zenburn.lua
+++ lua/lualine/themes/zenburn.lua
@@ -3,37 +3,37 @@ return {
     normal = {
         a = { bg = c.SpecialComment.fg, fg = c.SignColumn.fg, gui = "bold" },
         b = { bg = c.LineNr.fg, fg = c.Normal.fg },
-        c = { bg = "NONE", fg = "#dcdccc" },
+        c = { bg = c.LineNr.fg, fg = "#dcdccc" }, -- Match bg with section b
         z = { bg = c.Delimiter.fg, fg = c.SignColumn.fg },
     },
     insert = {
         a = { bg = c.Function.fg, fg = c.SignColumn.fg, gui = "bold" },
         b = { bg = c.LineNr.fg, fg = c.Normal.fg },
-        c = { bg = "NONE", fg = "#dcdccc" },
+        c = { bg = c.LineNr.fg, fg = "#dcdccc" }, -- Match bg with section b
         z = { bg = c.Delimiter.fg, fg = c.SignColumn.fg },
     },
     visual = {
         a = { bg = c.Define.fg, fg = c.SignColumn.fg, gui = "bold" },
         b = { bg = c.LineNr.fg, fg = c.Normal.fg },
-        c = { bg = "NONE", fg = "#dcdccc" },
+        c = { bg = c.LineNr.fg, fg = "#dcdccc" }, -- Match bg with section b
         z = { bg = c.Delimiter.fg, fg = c.SignColumn.fg },
     },
     replace = {
         a = { bg = c.Number.fg, fg = c.SignColumn.fg, gui = "bold" },
         b = { bg = c.LineNr.fg, fg = c.Normal.fg },
-        c = { bg = "NONE", fg = "#dcdccc" },
+        c = { bg = c.LineNr.fg, fg = "#dcdccc" }, -- Match bg with section b
         z = { bg = c.Delimiter.fg, fg = c.SignColumn.fg },
     },
     command = {
         a = { bg = c.Boolean.fg, fg = c.SignColumn.fg, gui = "bold" },
         b = { bg = c.LineNr.fg, fg = c.Normal.fg },
-        c = { bg = "NONE", fg = "#dcdccc" },
+        c = { bg = c.LineNr.fg, fg = "#dcdccc" }, -- Match bg with section b
         z = { bg = c.Delimiter.fg, fg = c.SignColumn.fg },
     },
     inactive = {
         a = { bg = c.Delimiter.fg, fg = c.SignColumn.fg, gui = "bold" },
         b = { bg = c.LineNr.fg, fg = c.Normal.fg },
-        c = { bg = "NONE", fg = "#dcdccc" },
+        c = { bg = c.LineNr.fg, fg = "#dcdccc" }, -- Match bg with section b
         z = { bg = c.Delimiter.fg, fg = c.SignColumn.fg },
     },
 }
