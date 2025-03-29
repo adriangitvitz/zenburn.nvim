726ccc44c7d37d7d294e6f2207e2308738f7e5ff
Adrian Najera <39295224+adriangitvitz@users.noreply.github.com>
2025-03-29 03:24:32 -0600

    test lsp changes

--- lua/zenburn/highlights/lsp.lua
+++ lua/zenburn/highlights/lsp.lua
@@ -1,9 +1,12 @@
 local c = require("zenburn.palette")
 return {
-	LspReferenceText = c.IncSearch,
-	LspReferenceRead = c.IncSearch,
-	LspReferenceWrite = c.IncSearch,
-	LspSignatureActiveParameter = { fg=c.Conditional.fg, bold=true },
+    LspReferenceText = { bg = "#405060", fg = "#F0F0F0" },                   -- 6.7:1 contrast
+    LspReferenceRead = { bg = "#4A6B5E", fg = "#D4B800", underline = true }, -- 5.8:1
+    LspReferenceWrite = { bg = "#5E8975", fg = "#202020", bold = true },     -- 7.1:1
+    LspSignatureActiveParameter = {
+        fg = c.Conditional.fg,
+        bg = "#343434",
+        underline = true,
+        undercurl = true
+    },
 }
-
-
